2b2e6b4edbe9088505e4cce20eb70313c4b3d6e3
Henry Nawrocki <hdev@sztanski.com>
2022-06-23 12:11:06 -0500

    Add method logic to :fizzbuzz

--- solver.rb
+++ solver.rb
@@ -7,7 +7,9 @@
 #  Solver.reverse('hello') => 'olleh'
 #    @params [String] string
 #    @return [String]
-
+#  Solver.fizzbuzz(15) => 'FizzBuzz'
+#    @params [Integer] number
+#    @return [String]
 class Solver
   def self.factorial(num)
     raise ArgumentError if num.negative?
@@ -18,5 +20,17 @@ class Solver
 
   def self.reverse(str)
     str.reverse
-  end 
+  end
+
+  def self.fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'FizzBuzz'
+    elsif (num % 3).zero?
+      'Fizz'
+    elsif (num % 5).zero?
+      'Buzz'
+    else
+      num.to_s
+    end
+  end
 end
