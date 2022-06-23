adc21bbc1819faf118fabeb268b325c2cef45fb4
Henry Nawrocki <hdev@sztanski.com>
2022-06-23 11:45:26 -0500

    Add methods and logic to fulfill tests

---
+++ solver.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# Solver
+#  Solver.factorial(5) => 120
+#    @params [Integer] number
+#    @return [Integer]
+class Solver
+  def self.factorial(num)
+    raise ArgumentError if num.negative?
+    return 1 if num.zero? || num == 1
+
+    num * factorial(num - 1)
+  end
+end
